9e2b780846af194ca4920a44783377cb9c2bbd60
Andrés Darín <andres.darin05@gmail.com>
2025-07-26 18:30:00 -0300

    minor fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -7,12 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
     menuAbierto = false;
+    previousScrollPosition = window.pageYOffset;
+    headerVisible = true;
 
-    toggleMenu() {
+    toggleMenu(): void {
         this.menuAbierto = !this.menuAbierto;
     }
 
-    hideBurguer() {
+    hideBurguer(): void {
         this.menuAbierto = false;
+    }
+
+    navigateToSection(event: Event, sectionId: string): void {
+        event.preventDefault(); // evita salto directo
+        this.hideBurguer(); // cierra el menú hamburguesa
+
+        // Esperamos un poco para dar tiempo a cerrar animaciones
+        setTimeout(() => {
+            const section = document.getElementById(sectionId);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        }, 200);
+    }
+
+    @HostListener('window:scroll', [])
+    onWindowScroll(): void {
+        const currentScroll = window.pageYOffset;
+
+        if (currentScroll > this.previousScrollPosition && currentScroll > 100) {
+            this.headerVisible = false;
+        } else {
+            this.headerVisible = true;
+        }
+
+        this.previousScrollPosition = currentScroll;
     }
 }
